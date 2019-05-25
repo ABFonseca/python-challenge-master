68c597607aaddb15120f082fead84fffbf25ce3c
Antonio Fonseca <fonsecabb@gmail.com>
2019-05-25 17:22:40 +0100

    Add spacing between functions

--- test_search.py
+++ test_search.py
@@ -11,12 +11,16 @@ class TestTrieSearchMethods(unittest.TestCase):
 
     def test_not_found(self):
         self.assertEqual( trie.find_autocomplete(self.root, 'ABCD1234'), [])
+        
     def test_exact_match(self):
         self.assertEqual( trie.find_autocomplete(self.root, 'WhatsApp Messenger'), ['WhatsApp Messenger'])
+        
     def test_unique_match(self):
         self.assertEqual( trie.find_autocomplete(self.root, 'What'), ['WhatsApp Messenger'])
+        
     def test_multiple_match(self):
         self.assertEqual( len(trie.find_autocomplete(self.root, 'W')), 4)
+        
     def test_space_on_prefix(self):
         self.assertEqual( trie.find_autocomplete(self.root, 'Facebook L'), ['Facebook Lite'])
         
