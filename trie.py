bd4341568e0a3c4ebaf511be95c964a04b6b13f9
Antonio Fonseca <fonsecabb@gmail.com>
2019-05-25 11:26:39 +0100

    Add searching methods

--- trie.py
+++ trie.py
@@ -13,6 +13,16 @@ class TrieNode(object):
         self.word_finished = False
         # How many times this character appeared in the addition process
         self.counter = 1
+        
+    def all_words(self, prefix):
+        """
+        Gives all word completition starting on the node and assuming the path to the node is the string passed in prefix
+        """
+        if self.word_finished:
+            yield prefix
+
+        for child in self.children:
+            yield from child.all_words(prefix + child.char)
     
 
 def add(root, word: str):
@@ -71,3 +81,39 @@ def find_prefix(root, prefix: str) -> Tuple[bool, int]:
     # prefix
     return True, node.counter
 
+def find_autocomplete(root, prefix: str) -> list:
+    """
+    Check and return 
+      1. If the prefix exsists in any of the words we added so far
+      2. If yes then a list of all the autocompletetions of the prefix
+      3. An empty list if the prefix is not found
+    """
+    node = root
+    # If the root node has no children, then return empty list.
+    # Because it means we are trying to search in an empty trie
+    if not root.children:
+        return []
+    for char in prefix:
+        char_not_found = True
+        # Search through all the children of the present `node`
+        for child in node.children:
+            if child.char == char:
+                # We found the char existing in the child.
+                char_not_found = False
+                # Assign node as the child containing the char and break
+                node = child
+                break
+        # Return empty list anyway when we did not find a char.
+        if char_not_found:
+            return []
+    # Well, we are here means we have found the prefix. Return true to indicate that
+    # And also the counter of the last node. This indicates how many words have this
+    # prefix
+    
+    return list(node.all_words(prefix))
+    
+
+    
+    
+    
+    
