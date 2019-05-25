f8af4b8208d4c92a0a0c65413918b70d69b77624
Antonio Fonseca <fonsecabb@gmail.com>
2019-05-25 15:25:24 +0100

    FIX API request decode from bytes to unicode string

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 def load_data(root, filename):
     """
-    Loads the data in the file specified in the filename on the trie passed on root parameter
+    Loads the data in the file specified in the filename on the trie root parameter
     """
     try:
         with open(args.filename, 'r') as csv_file:
@@ -36,14 +36,16 @@ def read_input():
 
 @app.route('/autocomplete', methods=["Post"])
 def get_autocomplete():
-    results = trie.find_autocomplete(root, request.get_data())
+    prefix = request.get_data()
+    results = trie.find_autocomplete(root, prefix.decode("utf-8"))
+    print (results) #TODO for debug only
     return json.dumps("No Data")
 
 if __name__ == "__main__":
     args = read_input()
     
     root = load_data(root, args.filename)
-    
-    app.run(debug=False, host="127.0.0.1", port=8080, threaded=True)
+        
+    app.run(debug=True, host="127.0.0.1", port=8080, threaded=True)
 
     
